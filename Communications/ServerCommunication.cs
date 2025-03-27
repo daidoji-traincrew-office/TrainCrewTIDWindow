54f112f3aee5217dc8e3e633c7a66e96b970e1e0
gimyonabetechi <gimyo.nabetateyamatonnel+github@gmail.com>
2025-03-27 23:15:50 +0900

    サーバ通信失敗時ダイアログを出すように

--- Communications/ServerCommunication.cs
+++ Communications/ServerCommunication.cs
@@ -20,6 +20,8 @@ namespace TrainCrewTIDWindow.Communications
         
         internal event Action<ConstantDataToServer>? DataUpdated;
 
+        private bool error = false;
+
         /// <summary>
         /// アプリケーション用のホスト構築
         /// </summary>
@@ -45,6 +47,7 @@ namespace TrainCrewTIDWindow.Communications
 
             try {
                 _window.LabelStatusText = "Status：サーバ認証待機中";
+                error = false;
 
                 // 認証フローの開始
                 var result = await _service.ChallengeInteractivelyAsync(new() {
@@ -58,11 +61,11 @@ namespace TrainCrewTIDWindow.Communications
                 });
                 var token = resultAuth.BackchannelAccessToken!;
 
-                _window.LabelStatusText = "Status：サーバ認証成功";
                 await ConnectAsync(token);
             }
 
             catch (OperationCanceledException) {
+                error = true;
 
                 _window.LabelStatusText = "Status：サーバ認証失敗（タイムアウト）";
                 DialogResult result = MessageBox.Show($"サーバ認証中にタイムアウトしました。\n再認証しますか？", "サーバ認証失敗（タイムアウト） | TID - ダイヤ運転会", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
@@ -73,17 +76,19 @@ namespace TrainCrewTIDWindow.Communications
 
             catch (OpenIddictExceptions.ProtocolException exception) when (exception.Error is OpenIddictConstants.Errors
                                                                                .AccessDenied) {
+                error = true;
 
                 _window.LabelStatusText = "Status：サーバ認証失敗（拒否）";
                 TaskDialog.ShowDialog(new TaskDialogPage {
                     Caption = "サーバ認証失敗（拒否） | TID - ダイヤ運転会",
                     Heading = "サーバ認証失敗（拒否）",
                     Icon = TaskDialogIcon.Error,
-                    Text = "サーバ認証は拒否されました。\n入鋏されていない可能性があります。\n司令主任に連絡してください。\n再試行する場合はアプリケーションを再起動してください。"
+                    Text = "サーバ認証は拒否されました。\n入鋏されていない可能性があります。\n入鋏を受け、必要な権限を取得してください。\n再試行する場合はアプリケーションを再起動してください。"
                 });
             }
 
             catch (Exception exception) {
+                error = true;
                 Debug.WriteLine(exception);
                 _window.LabelStatusText = "Status：サーバ認証失敗";
                 DialogResult result = MessageBox.Show($"サーバ認証に失敗しました。\n再認証しますか？\n\n{exception.Message}\n{exception.StackTrace})", "サーバ認証失敗 | TID - ダイヤ運転会", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
@@ -100,31 +105,38 @@ namespace TrainCrewTIDWindow.Communications
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
         private async Task ConnectAsync(string token) {
+
+            // HubConnectionの作成
+            connection = new HubConnectionBuilder()
+                .WithUrl($"{ServerAddress.SignalAddress}/hub/TID?access_token={token}")
+                .WithAutomaticReconnect() // 自動再接続
+                .Build();
+
             try {
-                // HubConnectionの作成
-                connection = new HubConnectionBuilder()
-                    .WithUrl($"{ServerAddress.SignalAddress}/hub/TID?access_token={token}")
-                    .WithAutomaticReconnect() // 自動再接続
-                    .Build();
 
                 // 接続開始
                 await connection.StartAsync();
-
-                _window.LabelStatusText = "Status：データ正常受信";
+                _window.LabelStatusText = "Status：サーバ認証成功";
             }
             catch (HttpRequestException e) when (e.StatusCode == HttpStatusCode.Forbidden) {
+                error = true;
                 _window.LabelStatusText = "Status：サーバ認証失敗（拒否）";
                 TaskDialog.ShowDialog(new TaskDialogPage {
                     Caption = "サーバ認証失敗（拒否） | TID - ダイヤ運転会",
                     Heading = "サーバ認証失敗（拒否）",
                     Icon = TaskDialogIcon.Error,
-                    Text = "サーバ認証は拒否されました。\n必要な権限を持っていない可能性があります。\n司令主任に連絡してください。\n再試行する場合はアプリケーションを再起動してください。"
+                    Text = "サーバ認証は拒否されました。\n必要な権限を持っていない可能性があります。\nログインしようとしたアカウントを確認してください。\n身に覚えが無い場合は司令主任に連絡してください。\n再試行する場合はアプリケーションを再起動してください。"
                 });
                 return;
             }
             catch (Exception exception) {
+                error = true;
                 Debug.WriteLine($"Server send failed: {exception.Message}");
-                _window.LabelStatusText = "Status：データ受信失敗";
+                _window.LabelStatusText = "Status：サーバ認証失敗";
+                DialogResult result = MessageBox.Show($"サーバ認証に失敗しました。\n再認証しますか？\n\n{exception.Message}\n{exception.StackTrace})", "サーバ認証失敗 | TID - ダイヤ運転会", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+                if (result == DialogResult.Yes) {
+                    await CheckUserAuthenticationAsync();
+                }
             }
         }
         
@@ -139,18 +151,38 @@ namespace TrainCrewTIDWindow.Communications
                 var trackCircuitList = data.TrackCircuitDatas;
                 JsonDebugLogManager.AddJsonText(JsonConvert.SerializeObject(trackCircuitList));
                 DataUpdated?.Invoke(data);
+
+                _window.LabelStatusText = "Status：データ正常受信";
             }
             catch (WebSocketException e) {
                 Debug.WriteLine($"Server send failed: {e.Message}");
                 Debug.WriteLine($"status {e.WebSocketErrorCode.ToString()}");
                 Debug.WriteLine(e.StackTrace);
-                _window.LabelStatusText = "Status：データ受信失敗";
+                if (!error) {
+                    error = true;
+                    _window.LabelStatusText = "Status：データ受信失敗";
+                    TaskDialog.ShowDialog(new TaskDialogPage {
+                        Caption = "データ受信失敗 | TID - ダイヤ運転会",
+                        Heading = "データ受信失敗",
+                        Icon = TaskDialogIcon.Error,
+                        Text = "データの受信に失敗しました。\n大変恐れ入りますが、アプリケーションの再起動をお願いします。"
+                    });
+                }
 
             }
             catch (Exception e) {
                 Debug.WriteLine($"Server send failed: {e.Message}");
                 Debug.WriteLine(e.StackTrace);
-                _window.LabelStatusText = "Status：データ受信失敗";
+                if (!error) {
+                    error = true;
+                    _window.LabelStatusText = "Status：データ受信失敗";
+                    TaskDialog.ShowDialog(new TaskDialogPage {
+                        Caption = "データ受信失敗 | TID - ダイヤ運転会",
+                        Heading = "データ受信失敗",
+                        Icon = TaskDialogIcon.Error,
+                        Text = "データの受信に失敗しました。\n大変恐れ入りますが、アプリケーションの再起動をお願いします。"
+                    });
+                }
             }
         }
     }
